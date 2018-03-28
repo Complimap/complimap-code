43288472e87923a045cb1ea27c425a7cd66a7b46
William Stevens <contact@wastevensv.com>
2018-03-27 22:25:36 -0400

    Major frontend rework. Now material design.

--- server/static/index.js
+++ server/static/index.js
@@ -7,15 +7,15 @@ function loadJSON(path, success, error)
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function()
     {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                if (success)
-                    success(JSON.parse(xhr.responseText));
-            } else {
-                if (error)
-                    error(xhr);
-            }
-        }
+	if (xhr.readyState === XMLHttpRequest.DONE) {
+	    if (xhr.status === 200) {
+		if (success)
+		    success(JSON.parse(xhr.responseText));
+	    } else {
+		if (error)
+		    error(xhr);
+	    }
+	}
     };
     xhr.open("GET", path, true);
     xhr.send();
@@ -28,15 +28,15 @@ function postJSON(url, data, success, error)
     xhr.setRequestHeader("Content-type", "application/json");
     xhr.onreadystatechange = function()
     {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                if (success)
-                    success(xhr.responseText);
-            } else {
-                if (error)
-                    error(xhr);
-            }
-        }
+	if (xhr.readyState === XMLHttpRequest.DONE) {
+	    if (xhr.status === 200) {
+		if (success)
+		    success(xhr.responseText);
+	    } else {
+		if (error)
+		    error(xhr);
+	    }
+	}
     };
     var data = JSON.stringify(data);
     xhr.send(data);
@@ -47,27 +47,37 @@ function addNode(resultsMap) {
     var initials = document.getElementById('initials').value;
     var address = document.getElementById('address').value;
     geocoder.geocode({'address': address}, function(results, status) {
-        if (status === 'OK') {
-            var data = {
-                'code': code, 'name': initials,
-                'lat': results[0].geometry.location.lat(),
-                'lng': results[0].geometry.location.lng(),
-            };
-            postJSON('/new_node', data,
-                     function(resp) {
-                         data = JSON.parse(resp);
-                         displayCode(data.message, data.next_code);
-                         initMap();
-                         map.setCenter(results[0].geometry.location);
-                     },
-                     function(xhr) {
-                         alert("Invalid code. Please try again.");
-                     }
-                    );
-        } else {
-            alert('Invalid location. Please try again.');
-        }
+	if (status === 'OK') {
+	    var data = {
+		'code': code, 'name': initials,
+		'lat': results[0].geometry.location.lat(),
+		'lng': results[0].geometry.location.lng(),
+	    };
+	    postJSON('/new_node', data,
+		     function(resp) {
+			 data = JSON.parse(resp);
+			 initPage();
+			 map.setCenter(results[0].geometry.location);
+			 displayCode(data.message, data.next_code);
+		     },
+		     function(xhr) {
+			 alert("Invalid code. Please try again.");
+		     }
+		    );
+	} else {
+	    alert('Invalid location. Please try again.');
+	}
     });
+}
+
+function hideForm() {
+    var cd = document.getElementById("code-display");
+    cd.style.display = "none";
+}
+
+function displayForm() {
+    var cd = document.getElementById("code-display");
+    cd.style.display = "none";
 }
 
 function displayCode(message, code) {
@@ -75,21 +85,21 @@ function displayCode(message, code) {
     var ctx = c.getContext("2d");
 
     var qr = new QRious({
-        value: window.location.host+'/#'+code,
-        background: 'white', // background color
-        foreground: 'black', // foreground color
-        level: 'L', // Error correction level of the QR code (L, M, Q, H)
-        mime: 'image/png', // MIME type used to render the image for the QR code
-        size: 300 // Size of the QR code in pixels.
+	value: window.location.host+'/#'+code,
+	background: 'white', // background color
+	foreground: 'black', // foreground color
+	level: 'L', // Error correction level of the QR code (L, M, Q, H)
+	mime: 'image/png', // MIME type used to render the image for the QR code
+	size: 300 // Size of the QR code in pixels.
     });
     qrcode = new Image();
     qrcode.src = qr.toDataURL();
     qrcode.onload = function() {
-        ctx.drawImage(qrcode,c.width-350,c.height-350);
+	ctx.drawImage(qrcode,c.width-350,c.height-350);
 
-        var button = document.getElementById('btn-download');
-        var dataURL = c.toDataURL("image/png");
-        button.href = dataURL;
+	var button = document.getElementById('btn-download');
+	var dataURL = c.toDataURL("image/png");
+	button.href = dataURL;
     };
 
     ctx.fillStyle = 'white';
@@ -106,55 +116,74 @@ function displayCode(message, code) {
     ctx.font = "30px Arial";
     ctx.fillText(code, 30, 220);
 
-    var cd = document.getElementById("code-display");
-    cd.style.display = "block";
-
-    var fp = document.getElementById("floating-panel");
-    fp.style.display = "none";
+    var modal = M.Modal.getInstance(document.getElementById("code-display"));
+    modal.open();
 }
 
-function initMap() {
+function initPage() {
     if(window.location.hash.split('#')[1]) {
-        document.getElementById('code').value = window.location.hash.split('#')[1]
+	document.getElementById('code').value = window.location.hash.split('#')[1]
     }
 
+
+    var elem = document.querySelector('.fixed-action-btn');
+    var instance = M.FloatingActionButton.init(elem);
+
+    var elem = document.querySelector('#code-display');
+    var instance = M.Modal.init(elem);
+
+    var elem = document.querySelector('#node-form');
+    var instance = M.Modal.init(elem);
+
     map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: {lat: 39.8283, lng: -78.57950}
+	zoom: 4,
+	center: {lat: 39.8283, lng: -78.57950},
+	disableDefaultUI: true,
+	zoomControl: true,
     });
 
     geocoder = new google.maps.Geocoder();
 
     document.getElementById('submit').addEventListener('click', function() {
-        addNode(map);
+	addNode(map);
     });
 
 
     loadJSON('/paths/all', function(paths) {
-        for(const p in paths) {
-            poly = new google.maps.Polyline({
-                strokeColor: '#000000',
-                strokeOpacity: 1.0,
-                strokeWeight: 2
-            });
-            poly.setMap(map);
+	for(const p in paths) {
+	    poly = new google.maps.Polyline({
+		strokeColor: '#000000',
+		strokeOpacity: 1.0,
+		strokeWeight: 2
+	    });
+	    poly.setMap(map);
 
-            for(const n in paths[p].nodes) {
-                var node = paths[p].nodes[n]
-                var path = poly.getPath();
-                var loc = new google.maps.LatLng(node.lat, node.lng);
+	    for(const n in paths[p].nodes) {
+		var node = paths[p].nodes[n]
+		var path = poly.getPath();
+		var loc = new google.maps.LatLng(node.lat, node.lng);
 
-                // Because path is an MVCArray, we can simply append a new coordinate
-                // and it will automatically appear.
-                path.push(loc);
+		// Because path is an MVCArray, we can simply append a new coordinate
+		// and it will automatically appear.
+		path.push(loc);
 
-                // Add a new marker at the new plotted point on the polyline.
-                var marker = new google.maps.Marker({
-                    position: loc,
-                    title: paths[p].message + " - " + node.person + " - " + node.time_created,
-                    map: map
-                });
-            }
-        }
+		// Add a new marker at the new plotted point on the polyline.
+		var marker = new google.maps.Marker({
+		    position: loc,
+		    title: paths[p].message + " - " + node.person + " - " + node.time_created,
+		    map: map,
+		    clickable: true
+		});
+		marker.info = new google.maps.InfoWindow({
+		    maxWidth: 200,
+		    content: '<div id="bodyContent">'+
+			'<p>' +  paths[p].message + '<br>' + node.person +
+			'<br>' + node.time_created + '</p>' + '</div>',
+		});
+		google.maps.event.addListener(marker, 'click', function() {
+		    this.info.open(map, this);
+		});
+	    }
+	}
     });
 }
